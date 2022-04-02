994fa338b08edf06380392973238317cdb23ed06
Wireless4024 <36229606+Wireless4024@users.noreply.github.com>
2022-04-02 17:57:37 +0700

    support multiple command

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/joho/godotenv"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -13,10 +14,24 @@ func main() {
 
 	app := fiber.New()
 
-	command := os.Getenv("COMMAND")
-	if len(command) == 0 {
-		panic("COMMAND env is missing")
+	images := make(map[string]string)
+
+	for _, kv := range os.Environ() {
+		arr := strings.Split(kv, "=")
+		if len(arr) > 0 {
+			key := strings.TrimSpace(arr[0])
+			value := strings.TrimSpace(arr[1])
+
+			if !strings.HasPrefix(key, "IMAGE_") || len(key) == 0 || len(value) == 0 {
+				continue
+			}
+
+			images[key[6:]] = value
+		}
 	}
+
+	/*j, _ := json.Marshal(images)
+	println(string(j))*/
 
 	token := os.Getenv("TOKEN")
 	if len(token) == 0 {
@@ -29,12 +44,17 @@ func main() {
 			reqToken = ctx.GetReqHeaders()["X-Gitlab-Token"]
 		}
 		if reqToken != token {
-
 			if err := ctx.SendStatus(400); err != nil {
 				return err
 			}
 			return errors.New("invalid token")
 		}
+
+		command := images[ctx.Params("image")]
+		if len(command) == 0 {
+			return errors.New("invalid image name")
+		}
+
 		cmd := exec.Command("bash", "-c", command)
 
 		if err := cmd.Run(); err != nil {
@@ -43,8 +63,8 @@ func main() {
 		return ctx.SendStatus(200)
 	}
 
-	app.Get("/", trigger)
-	app.Post("/", trigger)
+	app.Get("/:image", trigger)
+	app.Post("/:image", trigger)
 
 	listen := os.Getenv("LISTEN")
 	if len(listen) == 0 {
